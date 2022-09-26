273753c03970c3868f1a78d6e0dbd84f5641a2cc
Nick Pisano <pisano.nicholas@yahoo.com>
2022-09-25 22:08:17 -0700

    Fixed Bug and added about me

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          Nicholas Pisano
+        </h1>
+      <nav>
+        <a>LinkedIn</a>
+        <a>Resume</a>
+        <a>Experience</a>
+        <a>Projects</a>
+      </nav>
+      </header>
+      <div>
+        <h2>
+          About Me
+        </h2>
+        <p>
+          Hi, my name is Nicholas and I am a fluent english speaker. I am currently 23 years old living in Los Angeles County, California. I have many 
+          hobbies i love to surround myself with like video games, rock climbing and FPV Drone flying.
+          I think one of my greatest ability is also one of my greatest weaknesses, I strive always to carry my weight in anything I do in life. 
+          I have the same passion towards my coding. I'm 23 years old and I crave to learn new and different technologies everyday.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
