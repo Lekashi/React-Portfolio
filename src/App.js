b47e90f4a9cc432e4e52b383bb7039377066e0b8
Nick Pisano <pisano.nicholas@yahoo.com>
2022-09-30 12:38:32 -0700

    Added More Elements

--- src/App.js
+++ src/App.js
@@ -45,14 +45,32 @@ function App() {
       </div>
       <div>
         <h3 id="experience">Experience</h3>
-        <div>
           <h4>Technologies</h4>
+        <div>
+            <h4>Backend</h4>
           <ul>
-            Backend
           <li>Mongo</li>
           <li>MySQL</li>
           <li>NoSQL</li>
           <li>Socket.io</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Frontend</h4>
+          <ul>
+            <li>React.js</li>
+            <li>HTML5</li>
+            <li>C# & C++</li>
+            <li>Handlebars</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Languages</h4>
+          <ul>
+            <li>Python</li>
+            <li>C# & C++</li>
+            <li>Javascipt</li>
+            <li>CSS</li>
           </ul>
         </div>
       </div>
