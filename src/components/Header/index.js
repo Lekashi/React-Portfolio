758deccd4cb481a2a25eb9c6864f82c65f6ed957
Nick Pisano <pisano.nicholas@yahoo.com>
2022-10-01 19:08:39 -0700

    Added CSS to Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,34 @@
 import React from "react";
-import "./index.css"
+import "./index.css";
 
 const Header = () => {
   return (
     <header>
-    <h1>Nicholas Pisano</h1>
-    <nav>
-      <a
-        href="https://www.linkedin.com/in/nicholas-pisano-042a1423a/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        LinkedIn
-      </a>
-      <a
-        href="https://github.com/Lekashi"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Github
-      </a>
-      <a href="../../../public/Resume.docx" download>Resume</a>
-      <a href="#experience">Experience</a>
-      <a href="#projects">Projects</a>
-    </nav>
-  </header>
+      <h1>Nicholas Pisano</h1>
+      <nav>
+        <a
+          className="navBtn"
+          href="https://www.linkedin.com/in/nicholas-pisano-042a1423a/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a
+          className="navBtn"
+          href="https://github.com/Lekashi"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+        <a className="navBtn" href="../../../public/Resume.docx" download>
+          Resume
+        </a>
+        <a className="navBtn" href="#experience">Experience</a>
+        <a className="navBtn" href="#projects">Projects</a>
+      </nav>
+    </header>
   );
 };
 
