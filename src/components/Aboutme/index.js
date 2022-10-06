0d5e94eb58001061c5bebc65bfa59de22c5f4211
Nick Pisano <pisano.nicholas@yahoo.com>
2022-10-05 21:08:17 -0700

    Added CSS to Aboutme Component

--- src/components/Aboutme/index.js
+++ src/components/Aboutme/index.js
@@ -1,18 +1,23 @@
 import React from "react";
+import imgMe from "../../images/me1.jpg"
+import "./index.css";
 
 const Aboutme = () => {
   return (
-    <div>
+    <div className="aboutMe">
+    <img src={imgMe} alt="Nicholas's Portrait shot" width="500" height="400">
+    </img>
+    <div className="aboutMeText">
     <h2>About Me</h2>
-    <p>
-      Hi, my name is Nicholas. I am currently 23 years old living in Los
+    <p className="whoIAm">
+      Hi, my name is Nicholas Pisano. I am currently 23 years old living in Los
       Angeles County, California. I have many hobbies i love to surround
       myself with like video games, rock climbing and FPV Drone flying. I
       think one of my greatest ability is also one of my greatest
       weaknesses, I strive always to carry my weight in anything I do in
-      life. I have the same passion towards my coding. I'm 23 years old and
-      I crave to learn new and different technologies everyday.
+      life. I have the same passion towards my coding.
     </p>
+    </div>
   </div>
   );
 };
